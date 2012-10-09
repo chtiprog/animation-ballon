5ef17de04cf75e4ba76a34b3733f6c9a66159ec7
Anne-Catherine <ankat@ankat-R610.(none)>
2012-10-09 01:19:09 -0300

    Ajout d'une TODO Liste

--- src/FenetreBallonFoot.java
+++ src/FenetreBallonFoot.java
@@ -10,7 +10,7 @@ public class FenetreBallonFoot extends JFrame {
 
 	public FenetreBallonFoot(){
 		this.setTitle("Ballon de Foot");
-		this.setSize(700, 500);
+		this.setSize(800, 500);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 
@@ -30,15 +30,15 @@ public class FenetreBallonFoot extends JFrame {
 	private void go() {
 		x = pan.getPosX(); y = pan.getPosY();
 		// initialisation des variables servant de paramètres à la méthode go()
-		double v0 = 40;
-		double degres = 50;
+		double v0 = 30;
+		double degres = 20;
 		double angle = degres * Math.PI/180; // Conversion car la méthode cos et sin de Math est en radian
 		double vX = v0 * Math.cos(angle);  // vecteur vitesse horizontal
 		double vY = v0 * Math.sin(angle);  // vecteur vitesse vertical
 
 		while(x < ( pan.getWidth() - pan.getLargeur() ) ){ // tant qu'on ne touche pas le bord droit
 
-			if (y > pan.getHeight() - pan.getHauteur()) backY = true ; // On recule quand on touche le bord bas
+			if (y > pan.getHeight() - pan.getHauteur()) backY = true ; // On rebondit quand on touche le bord bas
 			if( !backY ){
 				tombe(angle, vX);
 			} // fin du if(ballon n'a pas atteint le bord bas)
@@ -90,7 +90,7 @@ public class FenetreBallonFoot extends JFrame {
  */
 	private void rebond(double angle, double vX) {
 		int t = 0;
-		while( y > hauteur * 80/100){
+		while( y > hauteur * 10/100){
 			++t;
 			x = x + (int) (vX * t); 
 			y = y - (int) ((vX * t) - (g *(t*t) / 2));
